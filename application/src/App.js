9fdcc745a8c0af4fa5ff5eb5adb7a328e158db7c
JHau21 <JackHau13@gmail.com>
2022-01-23 12:51:32 -0700

    Commented everything out but mostly finished messaging system

--- application/src/App.js
+++ application/src/App.js
@@ -1,17 +1,51 @@
+import { useEffect } from "react";
 import { SyncClient } from "twilio-sync";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	const testMessage = () => {
-		const token = "deltaupsilonsms-3011-dev.twil.io/sms-token";
-	};
+	// If I ever want to try out sync, I can try it out here.
+	// const tokenUrl = "http://deltaupsilonsms-3011-dev.twil.io/sms-token";
+	// const testMessage = async () => {
+	// 	const fetchToken = await fetch(tokenUrl);
+
+	// 	const { token } = await fetchToken.json();
+	// 	await new SyncClient(token);
+	// };
+	// const sendMessageUrl = "http://deltaupsilonsms-3011-dev.twil.io/sms-reply";
+
+	// const triggerFunc = () => {
+	// 	timerFunc();
+	// };
+
+	// const timerFunc = () => {
+	// 	setTimeout(async () => {
+	// 		const sendMessage = await fetch(sendMessageUrl);
+	// 		await sendMessage.json();
+	// 	}, 100000);
+	// 	triggerFunc();
+	// };
+
+	// const messageTest = async () => {
+	// 	const sendMessage = await fetch(sendMessageUrl);
+	// 	const response = await sendMessage.json();
+	// 	console.log(response);
+	// };
+
+	// useEffect(() => {
+	// 	triggerFunc();
+	// }, []);
 
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p onClick={() => console.log("Hello World!")}>
+				<p
+					onClick={() => {
+						// messageTest();
+						console.log("Hello!");
+					}}
+				>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
 			</header>
