0b9c7093b502fec0e79956d1957f546124794928
JHau21 <JackHau13@gmail.com>
2022-01-23 15:47:04 -0700

    SMS works, but need to figure out callback issue

--- application/src/App.js
+++ application/src/App.js
@@ -4,37 +4,33 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	// If I ever want to try out sync, I can try it out here.
-	// const tokenUrl = "http://deltaupsilonsms-3011-dev.twil.io/sms-token";
-	// const testMessage = async () => {
-	// 	const fetchToken = await fetch(tokenUrl);
+	const tokenUrl =
+		"http://deltaupsilonsms-3011-dev.twil.io/sms-token?To=+13038594840&From=+16075245881&Body=Jack, I Am Wathcing You";
+	const testMessage = async () => {
+		const fetchToken = await fetch(tokenUrl);
 
-	// 	const { token } = await fetchToken.json();
-	// 	await new SyncClient(token);
-	// };
-	// const sendMessageUrl = "http://deltaupsilonsms-3011-dev.twil.io/sms-reply";
+		const { token, status } = await fetchToken.json();
+		console.log(token, status);
+		await new SyncClient(token);
+	};
 
-	// const triggerFunc = () => {
-	// 	timerFunc();
-	// };
+	const sendMessageUrl =
+		"https://deltaupsilonsms-3011-dev.twil.io/sms-reply?To=+13038594840&From=+16075245881&Body=Jack, I Am Wathcing You";
 
-	// const timerFunc = () => {
-	// 	setTimeout(async () => {
-	// 		const sendMessage = await fetch(sendMessageUrl);
-	// 		await sendMessage.json();
-	// 	}, 100000);
-	// 	triggerFunc();
-	// };
+	const messageTest = async () => {
+		const sendMessage = await fetch(sendMessageUrl, {
+			mode: "no-cors",
+			method: "post",
+			headers: {
+				"Content-Type": "application/json",
+				"Access-Control-Allow-Origin": "http://localhost:3000",
+				"Access-Control-Allow-Headers": "Content-Type",
+				"Access-Control-Allow-Methods": "POST",
+			},
+		});
 
-	// const messageTest = async () => {
-	// 	const sendMessage = await fetch(sendMessageUrl);
-	// 	const response = await sendMessage.json();
-	// 	console.log(response);
-	// };
-
-	// useEffect(() => {
-	// 	triggerFunc();
-	// }, []);
+		const { status } = await sendMessage.json();
+	};
 
 	return (
 		<div className="App">
@@ -43,6 +39,7 @@ function App() {
 				<p
 					onClick={() => {
 						// messageTest();
+						testMessage();
 						console.log("Hello!");
 					}}
 				>
