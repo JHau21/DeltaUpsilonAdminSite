e2f1f9f5e736a3c7006cde847dd67e9e1648690a
JHau21 <JackHau13@gmail.com>
2022-01-24 13:23:12 -0700

    Finished sms messaging

--- application/src/App.js
+++ application/src/App.js
@@ -4,33 +4,59 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	const tokenUrl =
-		"http://deltaupsilonsms-3011-dev.twil.io/sms-token?To=+13038594840&From=+16075245881&Body=Jack, I Am Wathcing You";
 	const testMessage = async () => {
-		const fetchToken = await fetch(tokenUrl);
+		const messagingList = [
+			"+13038594840",
+			"+13039101892",
+			"+17143312909",
+			"+13039197221",
+			"+17135400389",
+			"+13035068995",
+			"+17207676808",
+			"+17203186244",
+		];
+		const messageBody =
+			"NoReply: Hi! This is an automessaging bot set up by Jack to remind you guys of important fraternity events, more on this at Chapter. Please text Jack and let him know if you will be attending Chapter! Jack's Number: (303) 859-4840";
+		let failedSend = [];
 
-		const { token, status } = await fetchToken.json();
-		console.log(token, status);
-		await new SyncClient(token);
-	};
+		let response;
 
-	const sendMessageUrl =
-		"https://deltaupsilonsms-3011-dev.twil.io/sms-reply?To=+13038594840&From=+16075245881&Body=Jack, I Am Wathcing You";
+		messagingList.map(async (number) => {
+			const sendUrl = `https://deltaupsilonsms-3011-dev.twil.io/sms-send-notification?To=${number}&From=+17206371593&Body=${messageBody}`;
+			response = await fetch(sendUrl);
 
-	const messageTest = async () => {
-		const sendMessage = await fetch(sendMessageUrl, {
-			mode: "no-cors",
-			method: "post",
-			headers: {
-				"Content-Type": "application/json",
-				"Access-Control-Allow-Origin": "http://localhost:3000",
-				"Access-Control-Allow-Headers": "Content-Type",
-				"Access-Control-Allow-Methods": "POST",
-			},
+			const { success, message } = await response.json();
+
+			if (!success) {
+				failedSend.push({ number, message });
+			}
 		});
 
-		const { status } = await sendMessage.json();
+		if (failedSend.length > 0) {
+			// Text myself about failure?
+			console.log(
+				"Not sure what to do yet, but eventually it would be good to notify myself of failure"
+			);
+		}
 	};
+
+	// const sendMessageUrl =
+	// 	"https://deltaupsilonsms-3011-dev.twil.io/sms-reply?To=+13038594840&From=+16075245881&Body=Jack, I Am Wathcing You";
+
+	// const messageTest = async () => {
+	// 	const sendMessage = await fetch(sendMessageUrl, {
+	// 		mode: "no-cors",
+	// 		method: "post",
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 			"Access-Control-Allow-Origin": "http://localhost:3000",
+	// 			"Access-Control-Allow-Headers": "Content-Type",
+	// 			"Access-Control-Allow-Methods": "POST",
+	// 		},
+	// 	});
+
+	// 	const { status } = await sendMessage.json();
+	// };
 
 	return (
 		<div className="App">
